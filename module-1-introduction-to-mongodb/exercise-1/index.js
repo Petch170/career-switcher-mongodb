44c5c3192b749c1f290ae666347e116304c7d01a
Your Name <you@example.com>
2024-01-25 16:18:46 +0700

    1

--- module-1-introduction-to-mongodb/exercise-1/index.js
+++ module-1-introduction-to-mongodb/exercise-1/index.js
@@ -1,4 +1,21 @@
 // go to link in google slide
 // create object in variable below
 
-const data = {};
+const data =  
+//จากหน้าลิ้ง shopee เงินปลอม
+{titile:"string",
+rate:"string|number",
+totalRating:"string|number",
+saler:"string|number",
+price:"string|number",
+discount:"string[]",
+wholesale:"string", //ขายส่ง
+shipping:{
+    destination:"string",
+    shippingCost:"number",
+},
+image:"string[]",
+sku:"string[]", //stock keeping unit ตรงcolor
+};
+
+mongodb+srv://np2forever:AjJj0qnVdWl3TEfx@jsd6.4rmu8mh.mongodb.net/
